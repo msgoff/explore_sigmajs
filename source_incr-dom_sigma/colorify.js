0b91271d85463de7145bf229bc0291c1e1d4fb6d
Palash <freelancedevops01@gmail.com>
2019-12-21 19:06:44 -0700

    square root and fractions nodes

--- source_incr-dom_sigma/colorify.js
+++ source_incr-dom_sigma/colorify.js
@@ -23,9 +23,39 @@ function getUnique(arr, comp) {
 
 	   return unique;
 	}
+	
+function getDOM(xmlstring) {
+    parser=new DOMParser();
+    return parser.parseFromString(xmlstring, "text/xml");
+}
+
+function remove_tags(node) {
+    var result = "";
+    var nodes = node.childNodes;
+    var tagName = node.tagName;
+    if (!nodes.length) {
+        if (node.nodeValue == "π") result = "pi";
+        else if (node.nodeValue == " ") result = "";
+        else result = node.nodeValue;
+    } else if (tagName == "mfrac") {
+        result = "("+remove_tags(nodes[0])+")/("+remove_tags(nodes[1])+")";
+    } else if (tagName == "msup") {
+        result = "Math.pow(("+remove_tags(nodes[0])+"),("+remove_tags(nodes[1])+"))";
+    } else for (var i = 0; i < nodes.length; ++i) {
+        result += remove_tags(nodes[i]);
+    }
+
+    if (tagName == "mfenced") result = "("+result+")";
+    if (tagName == "msqrt") result = "Math.sqrt("+result+")";
+
+    return result;
+}
+function stringifyMathML(mml) {
+   xmlDoc = getDOM(mml);
+   return remove_tags(xmlDoc.documentElement);
+}
+
 	  
-
-
 window.pushnode = function(dom_node){
 	elements.push(dom_node);
 
@@ -36,10 +66,21 @@ window.pushnode = function(dom_node){
 			$("#graph-container").empty();
 			var mathmlvalue = $("#node-input").val();
 			$(elements[4]).html(mathmlvalue);
+// start using parsing
+/*
+            var mathmltext1 = stringifyMathML(mathmlvalue).replace(/\s+/g, '');
+		    console.log(mathmltext1);
+*/
+// end using parsing
+
+//start using js
+
+			$("msqrt").prepend("&radic;");
+			$("mfrac").children().first().after(" / ");
+			$("mrow").append(")").prepend("(");
 			var mathmltext = $(elements[4]).text().replace(/\s+/g, '');
-
-
-
+		        mathmltext1 = mathmltext.replace('/', '  /  ');
+//end using js		      
 
 			var newnode = {
 				id: 'n'+mathmltext,
@@ -49,11 +90,6 @@ window.pushnode = function(dom_node){
 				size:2,
 				color:"#000"
 			}
-
-
-
-
-
 
 			g.nodes.push(newnode);
 
